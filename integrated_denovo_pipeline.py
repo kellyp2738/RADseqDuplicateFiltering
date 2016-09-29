f2f638c02f1c48c271cbfafe31414305bb4f5031
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-09-29 01:09:22 -0500

    Added regex for removing unnecessary whitespace in concatenate().

--- integrated_denovo_pipeline.py
+++ integrated_denovo_pipeline.py
@@ -271,6 +271,7 @@ def concatenate(read1, read2, out_name):
         with open(read2) as f2:
             with open(out_name, 'w') as outf:
                 fq_line = 1 # always start with the first line
+                rmwhite = re.compile(r'\s+')
                 for line in itertools.izip(f1, f2):
                     if fq_line == 1:
                         outf.write(line[0]+'\n') # just take the header from R1
