730d2b5d4aa682af29467c3e3d6eb2e33c5354e9
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-10-01 12:18:54 -0500

    Changed concatenate() to handle gzipped files.

--- integrated_denovo_pipeline.py
+++ integrated_denovo_pipeline.py
@@ -267,8 +267,8 @@ def parallel_concatenate(in_dir, regexR1, regexR2, out_dir):
         cP.join()
         
 def concatenate(read1, read2, out_name):
-    with open(read1) as f1:
-        with open(read2) as f2:
+    with gzip.open(read1) as f1:
+        with gzip.open(read2) as f2:
             with open(out_name, 'w') as outf:
                 fq_line = 1 # always start with the first line
                 rmwhite = re.compile(r'\s+')
