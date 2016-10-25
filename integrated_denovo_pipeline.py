7ecb9ce9f9045e9578a27ff381a3d6c5002ade71
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-10-25 00:12:42 -0500

    uncommented execution lines for trimming function

--- integrated_denovo_pipeline.py
+++ integrated_denovo_pipeline.py
@@ -470,10 +470,10 @@ def parallel_Trim(in_dir, out_dir, trimPath, first_base, last_base=None, suffix 
         print trim_call
         
         #trimProcess.append(mp.Process(target=Trim, args=(in_file, out_file, first_base, last_base, trimPath, execute)))
-        #commandline = Trim(in_file, out_file, trimPath, first_base, last_base, execute)
-        #processQueue.put(Work(commandline = commandline, shell = True), True, 360)
+        commandline = Trim(in_file, out_file, trimPath, first_base, last_base, execute)
+        processQueue.put(Work(commandline = commandline, shell = True), True, 360)
     
-    #processQueue.join()
+    processQueue.join()
     
     #for tP in trimProcess:
     #    tP.start()
